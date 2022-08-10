1546bf83c5dddc7511966587367cd7c6784d09df
AdrianSan2673 <87331204+AdrianSan2673@users.noreply.github.com>
2022-08-09 23:45:02 -0500

    adrian3

--- app/src/main/java/com/example/prueba/oooActivity.java
+++ app/src/main/java/com/example/prueba/oooActivity.java
@@ -11,7 +11,8 @@ public class oooActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ooo);
-        Toast.makeText(this,"Hola soy emanuel y adrian hola", Toast.LENGTH_LONG).show();
+        Toast.makeText(this,"Hola soy emanuel y adrian hola Mundo", Toast.LENGTH_LONG).show();
+        Toast.makeText(this,"Hola mundo", Toast.LENGTH_LONG).show();
 
 
     }
